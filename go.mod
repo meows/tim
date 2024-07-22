5a07405836d76392e988ded4494f071c93b2a7d3
Tim Engle <timothy.englew@gmail.com>
2024-07-22 16:39:43 -0700

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/timenglesf/personal-site
+
+go 1.22.1
+
+require (
+	github.com/a-h/templ v0.2.747 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+)
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'ui/template/**/*.templ'
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: ['Courier Prime', 'monospace'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    require('daisyui'),
+  ],
+  corePlugins: { preflight: true }
+}
+
