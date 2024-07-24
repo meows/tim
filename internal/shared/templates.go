6bb35d1a57db97cc063e2bf199e5afdb4f20b2e4
Tim Engle <timothy.englew@gmail.com>
2024-07-24 10:42:41 -0700

    create templateData for blog view

---
+++ internal/shared/templates.go
@@ -0,0 +1,35 @@
+package shared
+
+import (
+	"context"
+	"io"
+
+	"github.com/a-h/templ"
+	"github.com/timenglesf/personal-site/internal/models"
+)
+
+const DateLayout = "January 2, 2006"
+
+type adminTemplateData struct {
+	CurrentYear int
+	Blogs       any
+	Forms       any
+	Admin       models.User
+}
+
+type PostTemplateData struct {
+	CurrentYear int
+	// Posts       []models.Post
+	BlogPost models.Post
+	Form     any
+	// Flash       string
+	// CSRFtoken   string
+	//  User        models.User
+}
+
+func Unsafe(html string) templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) (err error) {
+		_, err = io.WriteString(w, html)
+		return
+	})
+}
