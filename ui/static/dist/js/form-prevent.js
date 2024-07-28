11e10f621d4ba03277d293e2e75bb2d809cba97a
Tim Engle <timothy.englew@gmail.com>
2024-07-27 17:01:32 -0700

    add js to prevent form submission on 'enter' keydown for title input

--- ui/static/dist/js/form-prevent.js
+++ ui/static/dist/js/form-prevent.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   const titleInput = document.getElementById('title-input');
+  const contentInput = document.querySelector("textarea")
   titleInput.addEventListener("keydown", e => {
     if (e.key === "Enter") {
       e.preventDefault();
+      contentInput.focus();
     }
   })
 });
